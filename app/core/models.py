85233df0fd3d1fa9a3c58158ca4bfe72889f66e4
angrycarrot <whitesaint12@gmail.com>
2023-09-23 17:02:46 +0200

    user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,54 @@
-from django.db import models # noqa
+"""
+DB models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin)
+
+
+class UserManager(BaseUserManager):
+    """Custom manager for users"""
+    def create_user(self, email, password=None, **extra_fields):
+        """creates, saves and returns a new user"""
+        if not email:
+            raise ValueError('Provide email!')
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """creates a superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.role = 'Admin'
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """represents user model"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    role_choices = (
+        ('Basic', 'Basic'),
+        ('Premium', 'Premium'),
+        ('Enterprise', 'Enterprise'),
+        ('Admin', 'Admin'),
+    )
+    role = models.CharField(max_length=50, choices=role_choices,
+                            default='Basic')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'

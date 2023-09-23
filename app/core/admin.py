1a6ce5079f0aea8b78ada065bba876f2aa9f1ba6
angrycarrot <whitesaint12@gmail.com>
2023-09-23 20:45:20 +0200

    first version of django admin, later will be created groups of users based on their roles to make easier work with permissions, also signals and custom commands will be added, but it will be after main functionality will be done

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,12 +4,44 @@ Django admin customization
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from . import models
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin page"""
     ordering = ['id']
-    list_display = ['email', 'name']
+    list_display = ['email', 'name', 'role']
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'role',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('important dates'), {'fields': ('last_login', )}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'role',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
